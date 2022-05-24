cf31b34564491a9f7c2eea47ec353103bac030d2
Marco Ledesma <marco.j.ledesma@gmail.com>
2022-05-24 08:59:13 -0400

    :pencil2: Fixing some typos and updates deploy config

--- components/SNavigation.vue
+++ components/SNavigation.vue
@@ -3,9 +3,9 @@
     <div class="max-w-4xl mx-auto flex items-center gap-2">
       <font-awesome-icon icon="sun" class="text-yellow-500 text-2xl animate-spin-slow"/>
       <span class="flex-grow text-xl font-semibold text-yellow-500">
-          Sunny Developments
+          Sunny Development
       </span>
-      <a href="#updates-soon" class="no-underline text-right">
+      <a href="#blog-soon" class="no-underline text-right">
         <p>
           Blog
         </p>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/sunny-development/'
+    base: '/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
