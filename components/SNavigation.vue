02cbc4e6432da56095da14feeb99d8bf12133dfd
Marco Ledesma <marco.j.ledesma@gmail.com>
2022-05-23 21:17:14 -0400

    :lipstick: adds personal favicon

--- components/SNavigation.vue
+++ components/SNavigation.vue
@@ -2,9 +2,9 @@
   <div class="bg-yellow-50 border-b-4 px-4 py-2 border-yellow-500">
     <div class="max-w-4xl mx-auto flex items-center gap-2">
       <font-awesome-icon icon="sun" class="text-yellow-500 text-2xl animate-spin-slow"/>
-      <span class="flex-grow text-xl font-semibold text-yellow-600">
+      <span class="flex-grow text-xl font-semibold text-yellow-500">
           Sunny Developments
-        </span>
+      </span>
       <a href="#updates-soon" class="no-underline text-right">
         <p>
           Blog
