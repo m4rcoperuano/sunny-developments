8a32ff0298f31ac1b05e60cbd9c794768c966c5a
Marco Ledesma <marco.j.ledesma@gmail.com>
2022-05-24 20:09:05 -0400

    :sparkles: activates blog

--- components/SNavigation.vue
+++ components/SNavigation.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="bg-yellow-50 border-b-4 px-4 py-2 border-yellow-500">
-    <div class="max-w-4xl mx-auto flex items-center gap-2">
+    <div class="max-w-4xl mx-auto flex items-center gap-2 py-1">
       <font-awesome-icon icon="sun" class="text-yellow-500 text-2xl animate-spin-slow"/>
       <span class="flex-grow text-xl font-semibold text-yellow-500">
           Sunny Development
       </span>
-      <a href="#blog-soon" class="no-underline text-right">
+      <a href="https://dev.to/m4rcoperuano" class="no-underline text-right">
         <p>
           Blog
-        </p>
-        <p class=" text-yellow-900 text-xs">
-          (coming soon!)
         </p>
       </a>
     </div>
